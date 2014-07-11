43ccdc75067117fe8c0fca4e9331024ddf2bcf23
Tucker Bickler <tbickler@homeaway.com>
2014-07-10 22:44:17 -0500

    regexp dogecoin: protocol

--- extension/src/highlighter.js
+++ extension/src/highlighter.js
@@ -1,6 +1,7 @@
 
 var regexp = new RegExp(/(D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32})/gm);
 var body_text = $('body').text();
+var wallet_menu_html = '<span class="wallet-menu">$1</span>';
 
 function getNodes() {
     var walker = document.createTreeWalker(
@@ -9,10 +10,8 @@ function getNodes() {
         null, 
         false
     );
-
     var node;
     var textNodes = [];
-
     while(node = walker.nextNode()) {
         textNodes.push(node);
     }
@@ -23,8 +22,17 @@ var nodes = getNodes();
 
 for (var i = 0; i < nodes.length; i+=1) {
     var node = nodes[i];
-    $(node).replaceWith(node.nodeValue.replace(regexp,'<span style="background-color:red">$1</span>'));
+    $(node).replaceWith(node.nodeValue.replace(regexp, wallet_menu_html));
 }
+
+var $links = $('[href^=dogecoin]');
+$links.each(function(i, el) {
+    var $el = $(el),
+        $children = $el.find('.wallet-menu');
+    if (!$children.length) {
+        $el.html($el.html().replace(/(.*)/, wallet_menu_html));
+    }
+})
 
 
 // var x = $('body').find("*").filter(function () {
